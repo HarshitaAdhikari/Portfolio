331ef3ff12ad46752c0a8ca8f7c34b5b19f30bca
HarshitaAdhikari <harshitaadhikari1234@gmail.com>
2026-03-30 15:38:33 +0530

    Updated index.html and fixed image paths

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import {
 import { HiOutlineMail } from "react-icons/hi";
 import { SiJavascript, SiTailwindcss, SiHtml5, SiGit } from "react-icons/si";
 
-const PROFILE_IMAGE = "public/image.png";
+const PROFILE_IMAGE = "/image.png";
 
 const SKILLS = [
   { label: "React JS",     icon: <FaReact     style={{ color: "#22d3ee" }} /> },
